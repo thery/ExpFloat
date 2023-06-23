0a6cc80c5e9a499b15d90f601cd44316a178a9b0
lrg <lrg@MacBook-Pro.local>
2023-06-23 08:59:23 +0200

    exactMul simplification

--- MULTmore.v
+++ MULTmore.v
@@ -100,48 +100,33 @@ Lemma exactMul (a b : R) :
   format a -> format b -> is_imul (a * b) (pow emin) ->
   format (RN (a * b) - a * b).
 move=> Fa Fb [z zE].
-have [->|ab_neq0] := Req_dec (a * b) 0.
-  by rewrite !(round_0, Rsimp01); apply: generic_format_0.
+have [rz rE]:( is_imul (RN (a*b)) (pow emin)).
+   by apply/is_imul_pow_round;exists z.
+have eE: (RN (a * b) - a * b) = IZR (rz -z) * pow emin.
+  by rewrite  rE zE minus_IZR; lra.
 have [pLab|pGab] := Rle_lt_dec (pow (emin + 2 * p - 1)) (Rabs (a * b)).
   by apply: mult_error_FLT.
 have F1 : Ulp.ulp beta fexp (pow (emin + 2 * p - 1)) = pow (emin + p).
   rewrite ulp_bpow; congr (pow _).
   by rewrite /fexp Z.max_l; lia.
-have F2 : Rabs (RN (a * b) - a * b) <= pow (emin + p).
-  rewrite -F1.
-  apply: Rle_trans (error_le_ulp _ _ _ _) _.
-  apply: ulp_le.
-  rewrite [X in _ <= X]Rabs_pos_eq //; first by lra.
-  apply: bpow_ge_0.
-have [zr Hzr] : 
-     exists zr, RN (a * b) = F2R ({| Fnum := zr; Fexp := emin |} : float).
-  have : format (RN (a * b)) by apply: generic_format_round.
-  rewrite /generic_format [X in _ = X -> _]/F2R /=.
-  set m := Ztrunc _; set e := cexp _ => H.
-  exists (m * (beta ^ (e - emin)))%Z.
-  rewrite H /F2R /= mult_IZR (IZR_Zpower beta).
-    rewrite Rmult_assoc -bpow_plus. 
-    by congr (_ * pow _); lia.
-  by rewrite /e /cexp /fexp; lia.
-have FF : RN (a * b) - a * b = F2R ({| Fnum := zr - z; Fexp := emin |} : float).
-  by rewrite Hzr zE /F2R /= minus_IZR; lra.
-have /Rle_lt_or_eq_dec [C1 | C2] := F2 ; last first.
-  apply: generic_format_abs_inv.
-  rewrite C2.
-  apply: generic_format_FLT.
-  by apply: FLT_format_bpow; lia.
-have F : Rabs (IZR (zr - z)) < pow p.
-  rewrite bpow_plus [_ * pow p]Rmult_comm in C1.
-  rewrite FF /F2R /= Rabs_mult in C1.
-  suff : 0 < pow emin by split_Rabs; nra.
-  by apply: bpow_gt_0.
-apply: generic_format_FLT.
-apply: FLT_spec FF _ _ => /=; last by lia.
-apply/lt_IZR.
-rewrite abs_IZR.
-rewrite -(IZR_Zpower beta) // in F.
-by suff : (0 < p)%Z by lia.
+have hu: Rabs (RN (a * b) - a * b) <= pow (emin + p).
+  apply/(Rle_trans _ (ulp beta fexp (a * b))); first by apply/error_le_ulp.
+  rewrite -F1; apply/ulp_le; rewrite (Rabs_pos_eq (pow _)); 
+    last by apply/bpow_ge_0.
+  lra.
+case :hu =>[ hu | ]; last first.
+  rewrite -(Rabs_pos_eq (pow _)); last by apply/bpow_ge_0.
+  by case/Rabs_eq_Rabs => ->; last apply/generic_format_opp;
+    apply/generic_format_FLT_bpow; lia.
+apply/generic_format_FLT.
+  apply/(FLT_spec _ _ _ _ ({| Fnum := rz - z; Fexp := emin |} : float)); 
+     rewrite /F2R //=; last lia.
+apply/lt_IZR; move:hu; rewrite {1}eE.
+rewrite Rabs_mult abs_IZR IZR_Zpower; last lia.
+rewrite (Rabs_pos_eq (pow _)); last by apply/bpow_ge_0.
+by rewrite bpow_plus; move:(bpow_gt_0 beta emin); nra.
 Qed.
+
 
 End Mult.
 
