0ebcc233309a90064ef1eceac9defe71d3396435
thery <Laurent.Thery@inria.fr>
2023-07-25 11:27:39 +0200

    some bounds of LOG2H and LOG2L

--- algoLog1.v
+++ algoLog1.v
@@ -49,18 +49,59 @@ Local Notation RN := (round beta fexp rnd).
 
 Definition LOG2H : float := Float _ 3048493539143 (- 42).
 
+Lemma LOG2HE : F2R LOG2H = 3048493539143 * (pow (- 42)).
+Proof. by rewrite /LOG2H /F2R /= /Z.pow_pos. Qed.
+
 Lemma format_LOG2H : format LOG2H.
 Proof.
 apply: generic_format_FLT.
 apply: FLT_spec (refl_equal _) _ _ => /=; lia.
 Qed.
 
+Lemma imul_LOG2H : is_imul LOG2H (pow (- 42)).
+Proof. by exists 3048493539143%Z. Qed.
+
+Lemma error_LOG2H : Rabs (LOG2H - ln 2) < pow (-44).
+Proof.
+by rewrite LOG2HE !pow_Rpower //; interval with (i_prec 54).
+Qed.
+
 Definition LOG2L : float := Float _ 544487923021427 (- 93).
+
+Lemma LOG2LE : F2R LOG2L = 544487923021427 * (pow (- 93)).
+Proof. by rewrite /LOG2L /F2R /= /Z.pow_pos. Qed.
 
 Lemma format_LOG2L : format LOG2L.
 Proof.
 apply: generic_format_FLT.
 apply: FLT_spec (refl_equal _) _ _ => /=; lia.
+Qed.
+
+Lemma imul_LOG2L : is_imul LOG2L (pow (- 93)).
+Proof. by exists 544487923021427%Z. Qed.
+
+Lemma error_LOG2L : Rabs (LOG2H + LOG2L - ln 2) < pow (- 102).
+Proof.
+by rewrite LOG2HE LOG2LE !pow_Rpower //; interval with (i_prec 120).
+Qed.
+
+Definition err8 e := Rabs (IZR e * LOG2H + IZR e * LOG2L - IZR e * ln 2).
+
+Lemma err8_0 : err8 0 = 0.
+Proof. by rewrite /err8 !Rsimp01. Qed.
+
+Lemma err8_bound e : (- 1074 <= e <= 1024)%Z -> err8 e <= Rpower 2 (- 91.949).
+Proof.
+move=> e8B.
+have {e8B}e8B1 : - 1074 <= IZR e <= 1024.
+  have [e8L e8R] := e8B.
+  by split; apply: IZR_le; lia.
+pose v := LOG2H + LOG2L - ln 2.
+have vB : - Rpower 2 (- 102.018) < v < 0.
+  by rewrite /v LOG2HE LOG2LE !pow_Rpower; split; interval with (i_prec 150).
+have -> : err8 e = Rabs (IZR e * v).
+  by congr (Rabs _); rewrite /v; lra.
+by interval with (i_prec 150).
 Qed.
 
 End Exp.
